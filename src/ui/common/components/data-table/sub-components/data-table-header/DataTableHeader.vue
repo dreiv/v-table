70f1a26e515ad995c311c205fe98c52f81a70886
Andrei Voicu <dreivoicu@gmail.com>
2022-04-05 13:18:52 +0300

    styling

--- src/ui/common/components/data-table/sub-components/data-table-header/DataTableHeader.vue
+++ src/ui/common/components/data-table/sub-components/data-table-header/DataTableHeader.vue
@@ -24,6 +24,11 @@ function onDrop(evt: DragEvent, to: string) {
   <div
     v-for="{ key, text } in context?.columns"
     :key="key"
+    :class="[
+      { [$style.isDragged]: key === draggedColumn },
+      { [$style.isDropTarget]: key === targetColumn },
+      $style.header,
+    ]"
     @drop="onDrop($event, key)"
     @dragover.prevent="targetColumn = key"
     @dragleave="targetColumn = null"
@@ -42,4 +47,31 @@ function onDrop(evt: DragEvent, to: string) {
 
 <style lang="scss" module>
 @import "@/ui/assets/styles/abstracts";
+
+.header {
+  display: flex;
+  gap: 8px;
+}
+
+.title {
+  flex: 1;
+
+  cursor: grab;
+  touch-action: none;
+}
+
+$viewportHeight: 100vh;
+.isDropTarget::before {
+  @include box(4px, $viewportHeight);
+  content: "";
+  position: absolute;
+
+  box-shadow: 0 $viewportHeight 0 var(--primary);
+  margin-top: -$viewportHeight;
+  opacity: 0.5;
+}
+
+.isDragged {
+  background-color: var(--active);
+}
 </style>
