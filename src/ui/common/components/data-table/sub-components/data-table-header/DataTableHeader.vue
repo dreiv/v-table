bc09b6a52844eb318e53efcc15aee2f5a18c75f3
Andrei Voicu <dreivoicu@gmail.com>
2022-04-05 10:43:39 +0300

    save

--- src/ui/common/components/data-table/sub-components/data-table-header/DataTableHeader.vue
+++ src/ui/common/components/data-table/sub-components/data-table-header/DataTableHeader.vue
@@ -1,13 +1,42 @@
 <script setup lang="ts">
-import { inject } from "vue";
+import { inject, ref } from "vue";
 import { DataTableKey } from "../../symbols";
 
 const context = inject(DataTableKey);
+
+const COLUMN_KEY = "column-key";
+const draggedColumn = ref();
+const targetColumn = ref();
+
+function startDrag(evt: DragEvent, from: string) {
+  evt.dataTransfer?.setData(COLUMN_KEY, (draggedColumn.value = from));
+}
+
+function onDrop(evt: DragEvent, to: string) {
+  const from = evt.dataTransfer?.getData(COLUMN_KEY)!;
+  if (to === from) return;
+
+  context?.value.onSwap(from, to);
+}
 </script>
 
 <template>
-  <div v-for="{ key, text, config: { width } } in context?.columns" :key="key">
-    {{ text }}
+  <div
+    v-for="{ key, text } in context?.columns"
+    :key="key"
+    @drop="onDrop($event, key)"
+    @dragover.prevent="targetColumn = key"
+    @dragleave="targetColumn = null"
+  >
+    <div
+      :class="$style.title"
+      draggable="true"
+      @dragstart="startDrag($event, key)"
+      @dragend="draggedColumn = targetColumn = null"
+    >
+      {{ text }}
+    </div>
+    <div>↕</div>
   </div>
 </template>
 
