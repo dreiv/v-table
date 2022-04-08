d2eb92f9765b020846ba67f6c76fe2ac06c7a5f7
Andrei Voicu <dreivoicu@gmail.com>
2022-04-08 10:49:48 +0300

    save

--- src/services/records.ts
+++ src/services/records.ts
@@ -1,8 +1,14 @@
 import { records } from "./mocks";
 
+const collator = new Intl.Collator("en", {
+  numeric: true,
+  sensitivity: "base",
+});
+const sorted = records.sort((a, z) => collator.compare(a.type, z.type));
+
 export function loadRecords(start: number, count: number): any {
   return {
-    records: records.slice(start, start + count),
-    total: records.length
-  }
+    records: sorted.slice(start, start + count),
+    total: sorted.length,
+  };
 }
--- src/ui/common/components/data-table/sub-components/DataTableRows.vue
+++ src/ui/common/components/data-table/sub-components/DataTableRows.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
-import { inject } from "vue";
+import { inject, computed } from "vue";
 import { DataTableKey } from "../symbols";
+
+const rows = computed(() => {
+  const { rows, groupBy } = context?.value!;
+
+  return !groupBy
+    ? rows
+    : rows.map((row, idx) => ({
+        ...row,
+        ...((!idx || rows[idx - 1][groupBy] !== row[groupBy]) && {
+          isHeader: true,
+        }),
+      }));
+});
 
 const context = inject(DataTableKey);
 </script>
 
 <template>
-  <template v-for="row in context?.rows" :key="row.id">
+  <template v-for="row in rows" :key="row.id">
+    <div v-if="row.isHeader" :class="$style.groupHeader">
+      {{ row[context?.groupBy!] }}
+    </div>
     <div v-for="{ key } in context?.columns">{{ row[key] }}</div>
   </template>
 </template>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+.groupHeader {
+  grid-column: 1 / -1;
+  background-color: lightgoldenrodyellow;
+}
+</style>
